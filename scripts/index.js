2f63fd364031a5baab65ba2966489cb35d755007
Timophey Shalonsky <shalonsky.tim@yandex.ru>
2022-04-29 18:05:29 +0300

    Fixes after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,32 @@
 const editBtn = document.querySelector('.profile__edit-button');
 const closeBtn = document.querySelector('.popup__close-button');
-const saveBtn = document.querySelector('.popup__save-button');
 const popup = document.querySelector('.popup');
 const profileName = document.querySelector('.profile__name');
 const profileText = document.querySelector('.profile__text');
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__text');
+let nameInput = formElement.querySelector('.popup__input_edit_name');
+let jobInput = formElement.querySelector('.popup__input_edit_text');
 
-nameInput.value = profileName.textContent;
-jobInput.value = profileText.textContent;
 
-function addProfile() {
-  popup.classList.toggle('popup_opened');
+
+function popupOpen() {  // Открывает попап и заносит в форму текущие значения профиля
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileText.textContent;
 }
 
-editBtn.addEventListener('click', addProfile);
-closeBtn.addEventListener('click', addProfile);
+function popupClose() {  // Закрывает попап
+  popup.classList.remove('popup_opened')
+}
 
-function formSubmitHandler (evt) {
+function formSubmitHandler (evt) {  // Заменяет текущие значения профиля на введенные в форму
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileText.textContent = jobInput.value;
-    addProfile();
+    popupClose();
 }
+
+editBtn.addEventListener('click', popupOpen);
+closeBtn.addEventListener('click', popupClose);
 
 formElement.addEventListener('submit', formSubmitHandler);
